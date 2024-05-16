a61126a350b6e1aed0a78268423907b5ef9394bc
nauhnguyen <huannguyenhoang270903@gmail.com>
2024-05-16 12:55:30 +0700

    product

--- homepage/script.js
+++ homepage/script.js
@@ -1,27 +1,3 @@
-// const next = document.querySelector('.next a');
-// const prev = document.querySelector('.prev a');
-// const comment = document.querySelector('#list-comment');
-// const commentItem = document.querySelectorAll('#list-comment .item');
-// let translateY = 0;
-// const itemHeight = 400; // Height of each comment item
-// const totalItems = commentItem.length;
-// const maxTranslateY = -itemHeight * (totalItems - 1);
-
-// next.addEventListener('click', function(event) {
-//     event.preventDefault();
-//     if (translateY > maxTranslateY) {
-//         translateY -= itemHeight;
-//         comment.style.transform = `translateY(${translateY}px)`;
-//     }
-// });
-
-// prev.addEventListener('click', function(event) {
-//     event.preventDefault();
-//     if (translateY < 0) {
-//         translateY += itemHeight;
-//         comment.style.transform = `translateY(${translateY}px)`;
-//     }
-// });
 
 const next = document.querySelector('.next')
 const prev = document.querySelector('.prev')
@@ -49,3 +25,5 @@ prev.addEventListener('click', function (event) {
     comment.style.transform = `translateY(${translateY}px)`
     count++
 })
+
+
